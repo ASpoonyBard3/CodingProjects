bea424ddc67935815920b06ecdb101d410ef161c
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-03 13:22:08 +0100

    added colours to each text to the return of fizz buzz, but the logic is incorrected.

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -10,16 +10,53 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < 151; i++)
             {
+                //if (i % 21 == 0)
+                //  Console.WriteLine("FizzBang");
                 if (i % 15 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
                     Console.WriteLine("FizzBuzz");
-                else if (i % 3 == 0)
-                    Console.WriteLine("Fizz");
+                }
+                else if (i % 13 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Fezz");
+                }
+
+                else if (i % 11 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Bong");
+                }
+
+                else if (i % 7 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Bang");
+                }
+
+
                 else if (i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("Buzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Fizz");
+                }
+
                 else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine(i);
+                }
+
+
             }
             Console.ReadLine();
 
