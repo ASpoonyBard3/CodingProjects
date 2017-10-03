48097343b717682c2aee218657b452125937a1b4
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-03 15:55:39 +0100

    added Fezz feature for numbers that are multiples of 13

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -11,25 +11,43 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
 
-            for (int i = 1; i < 71; i++)
+            for (int i = 1; i < 144; i++)
             {
                 string result = System.String.Empty;
 
                 if (i % 3 == 0)
                 {
                     result = "Fizz";
-                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
                 }
 
                 if (i % 5 == 0)
                 {
                     result = result + "Buzz";
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                 }
 
                 if (i % 7 == 0)
                 {
                     result = result + "Bang";
                     Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                if (i % 11 == 0)
+                {
+                    result = "Bong";
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                if (i % 13 == 0)
+                {
+                    if (result.StartsWith("B") || result.StartsWith("b"))
+                    {
+                        result = "Fezz" + result;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        result = "Fezz";
+
                 }
 
                 if (result == System.String.Empty)
