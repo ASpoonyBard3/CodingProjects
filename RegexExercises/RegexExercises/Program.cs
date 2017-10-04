1dfd890568d74ff65ed54cdd207e4f468e61ec93
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-04 14:47:07 +0100

    counter of occurence of substrings using no regex

---
+++ RegexExercises/RegexExercises/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:\SampleText.txt";
+            string readText = File.ReadAllText(path).ToLower();
+
+
+            int counter = 0;
+
+            for (int i = 0; i < readText.Length - 2; i++)
+            {
+                string maybeTra = readText.Substring(i, 3);
+                bool contains = maybeTra.Contains("tra");
+                if (contains == true)
+                {
+                    counter += 1;
+                }
+
+            }
+            Console.WriteLine(counter);
+            Console.ReadLine();
+        }
+
+    }
+}
