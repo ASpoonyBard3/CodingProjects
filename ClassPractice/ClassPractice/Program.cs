8548ea9ce85f3fed8724391f6541b244cc8d8a48
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-05 11:01:21 +0100

    First Test class example. Returns nothing for Marissa.

---
+++ ClassPractice/ClassPractice/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Employee
+    {
+        private int _Age;
+        private string _Name;
+
+        public int Age
+        {
+            get { return _Age; }
+            set { _Age = value; }
+        }
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public void GetPayCheck()
+        {
+
+        }
+
+        public void Work()
+        {
+
+        }
+    }
+
+    public class Sample
+    {
+        public static void Maih()
+        {
+            Employee marissa = new Employee();
+
+            marissa.Work();
+            marissa.GetPayCheck();
+        }
+    }
+}
