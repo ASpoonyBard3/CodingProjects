635e932c59e97aae654f2b878c28baf015a217ea
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-05 12:13:33 +0100

    Created transaction class and transaction constructor.

--- SupportBank/SupportBank/Program.cs
+++ SupportBank/SupportBank/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SupportBank
 {
@@ -10,7 +11,31 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
+            var filePath = @"C:\Users\SJFow\Desktop\transactions2014.csv";
+            var contents = File.ReadAllLines(filePath);
+
+
 
         }
     }
+
+    public class Transaction
+    {
+        public DateTime Date;
+        public string From;
+        public string To;
+        public string Narrative;
+        public float Amount;
+        public Transaction(string date, string from, String to, String narrative, string amount)
+        {
+            Date = DateTime.Parse(date);
+            From = from;
+            To = to;
+            Narrative = narrative;
+            Amount = float.Parse(amount);
+        } 
+
+    }
 }
+
+
