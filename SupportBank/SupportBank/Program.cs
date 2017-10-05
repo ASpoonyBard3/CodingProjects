f78af6c9b41678add0dc63f71d1d43feac1307d4
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-05 14:44:02 +0100

    Added SupportBank to coding projects, can now loop through the lines in each file and return a list of all data in each transaction.

--- SupportBank/SupportBank/Program.cs
+++ SupportBank/SupportBank/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
 namespace SupportBank
 {
     class Program
@@ -12,10 +13,25 @@ namespace SupportBank
         static void Main(string[] args)
         {
             var filePath = @"C:\Users\SJFow\Desktop\transactions2014.csv";
-            var contents = File.ReadAllLines(filePath);
+            List<string> contents = File.ReadAllLines(filePath).ToList();
 
+            List<Transaction> TransactionList = new List<Transaction>();
 
+            foreach (var line in contents.GetRange(1, contents.Count - 1))
+            {
+                string [] splitLine = line.Split(',');
+                Transaction transaction = new Transaction(splitLine[0],splitLine[1],splitLine[2],splitLine[3],splitLine[4]);
+                TransactionList.Add(transaction);
+            }
 
+            foreach (var transaction in TransactionList)
+            {
+                
+                
+                //Console.WriteLine(transaction.From);
+            }
+
+            Console.ReadLine();
         }
     }
 
@@ -35,6 +51,16 @@ namespace SupportBank
             Amount = float.Parse(amount);
         } 
 
+    }
+    public class Person
+    {
+        public string Name;
+        public float Balance;
+        public Person(string name, string balance)
+        {
+            Name = name;
+            Balance = float.Parse(balance);
+        }
     }
 }
 
