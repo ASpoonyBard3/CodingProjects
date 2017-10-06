b2644f91f63f76f57f7e093c40e655a164865b19
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-06 09:44:01 +0100

    Went through code and added rather verbose comments to explain what each function does and it's purpose in the programme.

--- SupportBank/SupportBank/Program.cs
+++ SupportBank/SupportBank/Program.cs
@@ -12,11 +12,18 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
+            //takes in the files for parsing and converts the contents of the file into a string list
             var filePath = @"C:\Users\SJFow\Desktop\transactions2014.csv";
             List<string> contents = File.ReadAllLines(filePath).ToList();
 
+            //creates a new list object, for entering the list of transactions in the foreach loop 
+            //for spliting into individual elements
             List<Transaction> TransactionList = new List<Transaction>();
 
+            //why does this for loop exist? counts through the content of the content list, 
+            //using the range to stop before it reaches the end of the string list and breaks the code
+            //the string is then split across the elements of the transaction class, entering the elements 
+            //into a list
             foreach (var line in contents.GetRange(1, contents.Count - 1))
             {
                 string [] splitLine = line.Split(',');
@@ -24,13 +31,20 @@ namespace SupportBank
                 TransactionList.Add(transaction);
             }
 
+            /*this dictionary is populated by the below for each loop
+            with call to the person class which takes the person as string type and the balance amount as a float 
+            */
             Dictionary <String,Person> PersonalTransactions = new Dictionary<String, Person>();
 
             foreach (var transaction in TransactionList)
             {
+                //instantiates two new person classes, then the if statement populates the personal transaction dictionary
+                //with the person name(if they don't exist already as a key) if they do exists it updates the transaction amount which is
+                //stored as the dictionary value
                 Person FromPerson = new Person(transaction.From,-transaction.Amount);
                 Person ToPerson = new Person(transaction.To, transaction.Amount);
 
+                //use FromPerson class to populate subtracts from the person's balance. 
                 if (PersonalTransactions.ContainsKey(FromPerson.Name))
                 {
                     PersonalTransactions[FromPerson.Name].Balance -= transaction.Amount;
@@ -40,6 +54,7 @@ namespace SupportBank
                     PersonalTransactions.Add(FromPerson.Name, FromPerson);
                 }
 
+                //use ToPerson class to populate additions to the person's balance. 
                 if (PersonalTransactions.ContainsKey(ToPerson.Name))
                 {
                     PersonalTransactions[ToPerson.Name].Balance += transaction.Amount;
@@ -50,6 +65,7 @@ namespace SupportBank
                 }
             }
 
+            //reads in the the personal transaction class by each person key and writes the output to the console.
             foreach (var person in PersonalTransactions)
             {
                 Console.WriteLine("{0} account balance is {1}.", person.Key, person.Value.Balance);
